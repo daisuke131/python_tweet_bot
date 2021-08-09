7e14f25340e4f7cf7b0e622e4a96698cef5b1fe0
daisuke <dada.00131@gmail.com>
2021-08-09 17:46:11 +0900

    アフィリ用URL追加

--- run_spread.py
+++ run_spread.py
@@ -1,4 +1,3 @@
-import os
 from concurrent.futures import ThreadPoolExecutor
 
 from gspread.models import Worksheet
@@ -7,7 +6,6 @@ from common.beutifulsoup import fetch_soup
 from common.ggl_spreadsheet import Gspread
 from common.tweet import Tweet
 
-CSV_PATH = os.path.join(os.getcwd(), "csv/urls.csv")
 THREAD_COUNT = None
 
 
@@ -19,17 +17,22 @@ class BuyableTweet:
     def buy_tweet(self):
         row_count = 0
         with ThreadPoolExecutor(THREAD_COUNT) as executor:
-            for hash_tag, url, is_buyable in self.ws.get_all_values():
+            for hash_tag, url, afili_url, is_buyable in self.ws.get_all_values():
                 row_count += 1
                 if row_count == 1:  # 一行目はカラム名なのでスルー
                     continue
                 executor.submit(
-                    self.tweet_decision, hash_tag, url, int(is_buyable), row_count
+                    self.tweet_decision,
+                    hash_tag,
+                    url,
+                    afili_url,
+                    int(is_buyable),
+                    row_count,
                 )
         print(row_count)
 
     def tweet_decision(
-        self, hash_tag: str, url: str, is_buyable: int, row_count: int
+        self, hash_tag: str, url: str, afili_url: str, is_buyable: int, row_count: int
     ) -> None:
         try:
             soup = fetch_soup(url)
@@ -40,7 +43,7 @@ class BuyableTweet:
             elif "item.rakuten" in url:
                 selector = ".cart-button.add-cart.new-cart-button"
                 platform = "楽天市場"
-                title = soup.select_one(".catch_copy").get_text().replace("\n", "")
+                title = soup.select_one(".item_name > b").get_text().replace("\n", "")
             elif "books.rakuten" in url:
                 selector = ".new_addToCart"
                 platform = "楽天ブックス"
@@ -51,19 +54,26 @@ class BuyableTweet:
             if soup.select(selector):
                 if is_buyable == 0:
                     formated_hash_tag = self.formating_hash_tag(hash_tag)
+                    if formated_hash_tag:
+                        formated_hash_tag = "\n" + formated_hash_tag
                     self.tw.tweet(
-                        f"＼{platform}で再入荷中／\n{title}\n{url}\n{formated_hash_tag}"
+                        f"＼{platform}で購入できます！／\n{title}\n{afili_url}{formated_hash_tag}"
                     )
-                    print(f"＼{platform}で再入荷中／\n{title}\n{url}\n{formated_hash_tag}")
-                    self.ws.update_cell(row_count, 3, 1)
+                    print(
+                        f"＼{platform}で購入できます！／\n{title}\n{afili_url}{formated_hash_tag}"
+                    )
+                    self.ws.update_cell(row_count, 4, 1)
             else:
                 if is_buyable == 1:
-                    self.ws.update_cell(row_count, 3, 0)
+                    self.ws.update_cell(row_count, 4, 0)
         except Exception:
             print("失敗")
+            pass
 
     def formating_hash_tag(self, hash_tag: str) -> str:
         hash_tag_list = hash_tag.split()
+        if not hash_tag_list:
+            return ""
         new_hash_tag_list = []
         for s in hash_tag_list:
             new_hash_tag_list.append("#" + s)
