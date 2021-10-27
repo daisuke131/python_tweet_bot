5e807b4d091aa8ecb5f2f4cca53901417dfec5d9
daisuke <dada.00131@gmail.com>
2021-10-27 17:00:36 +0900

    値下げ通知機能実装

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ class BuyableTweet:
         self.sheet = self.gs.worksheet
         self.tw = Tweet()
         self.errors = []
-        # self.driver = Driver("PC")
         self.drivers = self.create_driver()
         
     def create_driver(self):
@@ -41,53 +40,51 @@ class BuyableTweet:
         for thread in threads:
             thread.join()
         
-        # row_count = 0
-        # for (
-        #     hash_tag,
-        #     url,
-        #     afili_url,
-        #     is_buyable,
-        # ) in self.gs.worksheet.get_all_values():
-        #     row_count += 1
-        #     if row_count == 1:  # 一行目はカラム名なのでスルー
-        #         continue
-        #     self.tweet_decision(
-        #         hash_tag,
-        #         url,
-        #         afili_url,
-        #         is_buyable,
-        #         row_count,
-        #     )
-        
     def buy_tweet_detail(self, url_datas, driver) -> None:
-        for (hash_tag, url, afili_url, is_buyable, row_count) in url_datas:
-            self.tweet_decision(hash_tag, url, afili_url, is_buyable, row_count, driver)
+        for (hash_tag, url, afili_url, price, is_buyable, row_count) in url_datas:
+            self.tweet_decision(hash_tag, url, afili_url, del_kanma(price), is_buyable, row_count, driver)
             
     def convert_class(self, class_str: str) -> str:
         class_str = class_str.replace(" ", ".")
         class_str = "." + class_str
         return class_str
         
-    # def driver_get(self, url: str) -> None:
-    #     if "amazon" in url:
-    #         self.driver.get(url)
-    #         sleep(random.randint(3, 15))
-    #     elif "books.rakuten" in url:
-    #         self.driver.get(url)
-    #         sleep(random.randint(3, 15))
+    def fetch_now_price(self, driver) -> int:
+        cls = self.convert_class("a-size-base a-color-price a-color-price")
+        now_price_int: int = 0
+        now_price_str: str = ""
+
+        if driver.find_elements_by_css_selector(
+            "span.a-price.a-text-price.a-size-medium"
+        ):
+            now_price_str = driver.find_element_by_css_selector(
+                "span.a-price.a-text-price.a-size-medium"
+            ).text
+            now_price_int = del_kanma(now_price_str)
+        elif driver.find_elements_by_css_selector("#priceblock_ourprice"):
+            now_price_str = driver.find_element_by_css_selector(
+                "#priceblock_ourprice"
+            ).text
+            now_price_int = del_kanma(now_price_str)
+        elif driver.find_elements_by_css_selector(f".a-color-base > {cls}"):
+            now_price_str = driver.find_element_by_css_selector(
+                f".a-color-base > {cls}"
+            ).text
+            now_price_int = del_kanma(now_price_str)
+        return now_price_str, now_price_int
 
     def tweet_decision(
         self,
         hash_tag: str,
         url: str,
         afili_url: str,
+        price: int,
         is_buyable: str,
         row_count: int,
         driver,
     ) -> None:
         try:
             is_buy: bool = False
-            # self.driver_get(url)
             if "amazon" in url:
                 driver.get(url)
                 sleep(random.randint(3, 8))
@@ -97,12 +94,8 @@ class BuyableTweet:
                 sleep(random.randint(3, 15))
             if "amazon" in url:
                 # アクセス制限食らったらreturn
-                # print(self.driver.driver.title)
                 print(driver.driver.title)
                 if (
-                    # "アクセス" in self.driver.driver.title
-                    # or "ご迷惑" in self.driver.driver.title
-                    # or self.driver.driver.title == "Amazon.co.jp"
                     "アクセス" in driver.driver.title
                     or "ご迷惑" in driver.driver.title
                     or driver.driver.title == "Amazon.co.jp"
@@ -110,41 +103,33 @@ class BuyableTweet:
                     print(f"{row_count}番目アクセス制限")
                     return
                 target_site = "Amazon"
-                # title = self.driver.find_element_by_css_selector(
-                
+                # タイトル取得
                 title = driver.find_element_by_css_selector(
                     "#productTitle"
                     ).text.replace("\n", "")
+                # 金額取得
+                now_price_str, now_price_int = self.fetch_now_price(driver)
                 """
                 #add-to-cart-buttonと#buy-now-buttonは普通の商品
                 #one-click-buttonは本
                 #buy-now-buttonのみは予約ボタン
                 """
                 if (
-                    # self.driver.find_elements_by_css_selector("#add-to-cart-button")
-                    # or self.driver.find_elements_by_css_selector("#one-click-button")
-                    # or self.driver.find_elements_by_css_selector("#buy-now-button")
                     driver.find_elements_by_css_selector("#add-to-cart-button")
                     or driver.find_elements_by_css_selector("#one-click-button")
                     or driver.find_elements_by_css_selector("#buy-now-button")
                 ):
                     is_buy = True
             elif "books.rakuten" in url:
-                # print(self.driver.driver.title)
                 print(driver.driver.title)
                 target_site = "楽天ブックス"
-                # title = self.driver.find_element_by_css_selector(
                 title = driver.find_element_by_css_selector(
                     "#productTitle"
                 ).text.replace("\n", "")
-                # if self.driver.find_elements_by_css_selector(".new_addToCart"):
                 if driver.find_elements_by_css_selector(".new_addToCart"):
                     is_buy = True
             else:
                 target_site = "楽天市場"
-                # taget_url = url
-                # taget_urls = taget_url.split("/")
-                # item_code = f"{taget_urls[3]}:{taget_urls[4]}"
                 api_url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
                 params = {
                     "applicationId": "1019079537947262807",
@@ -162,29 +147,43 @@ class BuyableTweet:
 
             if is_buyable == "":
                 if is_buy:
-                    self.gs.update_cell(row_count, 4, 1)
+                    self.gs.update_cell(row_count, 5, 1)
                 else:
-                    self.gs.update_cell(row_count, 4, 0)
+                    self.gs.update_cell(row_count, 5, 0)
             else:
                 is_buyable = int(is_buyable)
                 if is_buy:
-                    # is_buyableが0ならTweet
-                    if is_buyable == 0:
-                        formated_hash_tag = self.formating_hash_tag(hash_tag)
-                        if formated_hash_tag:
-                            formated_hash_tag = "\n" + formated_hash_tag
+                    formated_hash_tag = self.formating_hash_tag(hash_tag)
+                    if formated_hash_tag:
+                        formated_hash_tag = "\n" + formated_hash_tag
+                    """
+                    指定した金額より低いとき値引きTweet
+                    値引き時はずっと通知するように
+                    値引き通知
+                    """
+                    if now_price_int < price:
                         self.tw.tweet(
-                            f"{now_time()}\n＼{target_site}で購入できます！／\n{title}"
+                            f"({now_time()})\n＼{target_site}で値引きされました！／\n{title}"
+                            + f"\n{now_price_str}\n{afili_url}{formated_hash_tag}"
+                        )
+                        print(
+                            f"({now_time()})\n＼{target_site}で値引きされました！／\n{title}"
+                            + f"\n{now_price_str}\n{afili_url}{formated_hash_tag}"
+                        )
+                    # is_buyableが0ならTweet(再入荷通知)
+                    if is_buyable == 0:
+                        self.tw.tweet(
+                            f"({now_time()})\n＼{target_site}で再入荷中！／\n{title}"
                             + f"\n{afili_url}{formated_hash_tag}"
                         )
                         print(
-                            f"{now_time()}\n＼{target_site}で購入できます！／\n{title}"
+                            f"({now_time()})\n＼{target_site}で再入荷中！／\n{title}"
                             + f"\n{afili_url}{formated_hash_tag}"
                         )
                         # 前回購入可を1にする
-                    self.gs.update_cell(row_count, 4, 1)
+                        self.gs.update_cell(row_count, 5, 1)
                 else:
-                    self.gs.update_cell(row_count, 4, 0)
+                    self.gs.update_cell(row_count, 5, 0)
             print(f"({now_time()}){row_count}番目成功")
         except Exception:
             self.errors.append([f"{row_count}番目", url])
@@ -216,7 +215,6 @@ def main():
         for i in range(100):
             try:
                 bt.buy_tweet()
-                # bt.driver.quit()
                 bt.output_errors()
                 print("==========")
             except Exception:
